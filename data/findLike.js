2683669507f45441898993717ecf329c6231e305
afkgeek <afkgeek@gmail.com>
2014-12-02 16:08:34 +0000

    Release v1.0

--- data/findLike.js
+++ data/findLike.js
@@ -11,12 +11,5 @@ if (btn != 'null') {
 		//Log not been liked
 		self.postMessage("Like the video");
 		btn.click(); //Click the button
-                //Perform checks to ensure that the button has been pressed
-                var btn = document.getElementById("watch-like");
-                if (btn.title === "Unlike") {
-                        self.postMessage("Video has been liked!");
-                } else {
-                        self.postMessage("Video like failed.");
-                }
 	};
 };
