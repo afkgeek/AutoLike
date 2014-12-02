4bda8a0bb1c3707279f13acc71374a26ed8b57a4
afkgeek <afkgeek@gmail.com>
2014-12-02 15:55:06 +0000

    Added Timer

--- data/findLike.js
+++ data/findLike.js
@@ -1,18 +1,22 @@
 var btn = document.getElementById("watch-like"); //Search for button within page elements
 //Perform further checks if found
 if (btn != 'null') {
-	//if video has not already been liked
+	//Check button title
 	if (btn.title === "Unlike") {
-		//Log message
+		//If it is equal to Unlike
+		//Log already liked message
 		self.postMessage("Video has already been liked.");
-		btn.click(); //Click the button
-		//Perform checks to ensure that the button has been pressed.
-		if (btn.title === "Unlike") {
-			self.postMessage("Video has been liked!");
-		} else {
-			self.postMessage("Video like failed.");
-		}
 	} else {
-		self.postMessage("Like the video"); //Log that it has already been done.
+		//Otherwise...
+		//Log not been liked
+		self.postMessage("Like the video");
+		btn.click(); //Click the button
+                //Perform checks to ensure that the button has been pressed
+                var btn = document.getElementById("watch-like");
+                if (btn.title === "Unlike") {
+                        self.postMessage("Video has been liked!");
+                } else {
+                        self.postMessage("Video like failed.");
+                }
 	};
 };
