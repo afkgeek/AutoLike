c1dd0106a92dd480fc9172c5eced3cd3ab4a03b4
afkgeek <afkgeek@gmail.com>
2014-12-02 12:29:37 +0000

    README Updated

---
+++ README.md
@@ -0,0 +1,5 @@
+# AutoLike #
+## Introduction ##
+AutoLike is an Add-on developed for Mozilla Firefox, which is designed to automatically "Like" videos watched on YouTube.
+## Bugs ##
+Please forward any bugs to: afkgeek@gmail.com
--- data/findLike.js
+++ data/findLike.js
@@ -1,8 +1,18 @@
-var btn = document.getElementById("watch-like");
+var btn = document.getElementById("watch-like"); //Search for button within page elements
+//Perform further checks if found
 if (btn != 'null') {
+	//if video has not already been liked
 	if (btn.title === "Unlike") {
+		//Log message
 		self.postMessage("Video has already been liked.");
+		btn.click(); //Click the button
+		//Perform checks to ensure that the button has been pressed.
+		if (btn.title === "Unlike") {
+			self.postMessage("Video has been liked!");
+		} else {
+			self.postMessage("Video like failed.");
+		}
 	} else {
-		self.postMessage("Like the video"); 
+		self.postMessage("Like the video"); //Log that it has already been done.
 	};
 };
