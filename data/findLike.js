9da8d92bc33e7312556206a9a6b20f1ed6a2c8ae
afkgeek <afkgeek@gmail.com>
2014-12-09 10:10:26 +0000

    v3.0 Update

--- data/findLike.js
+++ data/findLike.js
@@ -1,4 +1,8 @@
 self.port.on("findButton", function(bearhunt) {
 	var btn = document.getElementById(bearhunt); // find the like button
-	if (btn != 'null') self.port.emit("gotButton", btn); // return the button if found
+	if (btn === 'null') {
+		return 1;
+	} else {
+		self.port.emit("gotButton", btn); // return the button if found
+	}
 });
