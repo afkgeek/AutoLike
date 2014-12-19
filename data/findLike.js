0cf4266e2c83301492910f720af786e3a81b1290
AFK Geek <afkgeek@gmail.com>
2014-12-19 11:56:32 +0000

    comments added to code

--- data/findLike.js
+++ data/findLike.js
@@ -1,24 +1,13 @@
-	var bid = "watch-like";
-	console.log(" #3 Finding Button");
-	var btn = document.getElementById(bid);
-	if (btn != 'null') {
-		console.log(" #4 Button has been found ==> " + btn.title);
-		if (btn.title === "Unlike") {
-		} else {
-			btn.click();
-			console.log(" #5 CLICK");
-		}
+var bid = "watch-like"; // Set ID of Button to find
+console.log(" #3 Finding Button");
+var btn = document.getElementById(bid); // Search for button
+if (btn != 'null') {
+	// Continue if the button has been found
+	console.log(" #4 Button has been found ==> " + btn.title);
+	if (btn.title === "Unlike") {
+		// Do nothing if the video has been liked
+	} else {
+		btn.click(); // Click the button
+		console.log(" #5 CLICK");
 	}
-
-/*
-//self.port.on("findButton", function(bid) {
-	var bid = "watch-like";
-	var btn = document.getElementById(bid); // find the like button
-	if (btn != 'null') {
-		if (btn.title === "Unlike") {
-		} else {
-			btn.click();
-		};
-	};
-//});
-*/
+}
