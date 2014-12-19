342c7b9f6b29fe02a47c115c97e488590932ed7b
AFK Geek <afkgeek@gmail.com>
2014-12-19 15:07:12 +0000

    Fixed code errors. Timing bug still a problem

--- data/findLike.js
+++ data/findLike.js
@@ -1,15 +1,15 @@
 var bid = "watch-like"; // Set ID of Button to find
 console.log(" #3 Finding Button");
 var btn = document.getElementById(bid); // Search for button
-if (btn != 'null') {
+if (btn === 'null') {
+	// do nothing if not found
+} else {
 	// Continue if the button has been found
 	console.log(" #4 Button has been found ==> " + btn.title);
 	if (btn.title === "Unlike") {
 		// Do nothing if the video has been liked
-		return 2;
 	} else {
 		btn.click(); // Click the button
 		console.log(" #5 CLICK");
-		return 0;
 	}
 }
